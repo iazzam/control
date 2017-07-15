ad04699fa2a7ad0fb0e2a810dca49ac3302f9a28
Sidhant Mishra <s9mishra@uwaterloo.ca>
2017-07-14 23:29:09 -0400

    actually added code

--- src/Subsystems/ECBrake/ECBrake.h
+++ src/Subsystems/ECBrake/ECBrake.h
@@ -1,8 +1,21 @@
 //
-// Created by sidha on 2017-07-14.
+// Created by smishra on 2017-07-08.
 //
 
-#ifndef CONTROL_ECBRAKE_H
-#define CONTROL_ECBRAKE_H
+#ifndef ECBRAKE_ECBRAKE_H
+#define ECBRAKE_ECBRAKE_H
+
+#include "Configurations/State.h"
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+class ECBrake{
+private:
+    uint8_t relay;
+
+public:
+    void setup(uint8_t relay);
+    void control(bool state);
+};
 
 #endif //CONTROL_ECBRAKE_H
