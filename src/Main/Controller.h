712f3f925de294554b5d375f83e80be0f47bd7b8
Deep Dhillon <deep.dhill6@gmail.com>
2017-07-27 14:45:28 -0400

    library dependecy adjusted

--- src/Main/Controller.h
+++ src/Main/Controller.h
@@ -13,8 +13,6 @@
 #include <subsystems/MagWheels/MagWheels.h>
 #include <subsystems/DriveTrain/DriveTrain.h>
 #include <subsystems/ECBrakes/ECBrakes.h>
-#include <sensors/temp/IRTemp.h>
-#include <sensors/memory/SRAM.h>
 
 class Controller{
 	State &state;                   // state of the pod
